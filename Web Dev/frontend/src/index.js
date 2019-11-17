d449cf4b693a3648bb0b6bd31cf6c5a314b1c176
priyanshu2510 <priyanshu2510kr@gmail.com>
2019-11-17 07:11:21 +0530

    backend added

--- Web Dev/frontend/src/index.js
+++ Web Dev/frontend/src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
-
-import Inner from './components/inner';
-import Front from './components/front'
-ReactDOM.render(< Front/>, document.getElementById('root'));
+import App from './app';
+//import Inner from './components/inner';
+//import Front from './components/front'
+ReactDOM.render(< App/>, document.getElementById('root'));
 
 serviceWorker.unregister();
